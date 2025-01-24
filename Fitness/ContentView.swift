b67bec0e4f9ebe64d661732c1d974001b2f0182a
philippmCode <pdm_meyer@yahoo.com>
2025-01-24 18:36:02 +0100

    Wording change

--- Fitness/ContentView.swift
+++ Fitness/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, user!")
+            Text("Hello big dawg!")
         }
         .padding()
     }
